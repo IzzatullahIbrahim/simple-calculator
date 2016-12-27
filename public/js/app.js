2f496ae1d316a88ed2cb02d75a8030e7c4e7f27e
Zack <izzatullahibrahim@yahoo.com>
2016-12-27 10:24:48 -0700

    angular is working

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var app = angular.module ('MyApp',['ui.router']);
+
+app.config([
+    '$stateProvider',
+    function ($stateProvider){
+        $stateProvider
+            .state('home',{
+            url:'/',
+            templateUrl:'/templates/home.html'
+            })
+            .state('about',{
+            url:'/about',
+            templateUrl:'/templates/about.html'
+            })
+            .state('contact',{
+            url:'/contact',
+            templateUrl:'/templates/contact.html'
+            })
+            .state('calculator',{
+            url:'/calculator',
+            templateUrl:'/templates/home.html',
+            controller:'MyApp.CalculatorController'
+            })
+    }
+])
+
+app.controller ('MyApp.CalculatorController',[
+    '$scope',
+
+    function ($scope){
+        console.log('controller is working')
+    }
+])
